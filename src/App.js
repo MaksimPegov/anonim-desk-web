d124dde3a75d29092e99b4ea25d5119f2145d741
Maksim Pegov <maksim.peg@gmail.com>
2023-12-09 18:27:08 +0100

    Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="Header">
+        <h1>ANONYMOUS DESK</h1>
+        <hr></hr>
+        <h2>Write your anonymous message here!</h2>
+      </div>
+
+      <div className="MainBox">
+        {/* input */}
+        Message
+        {/* messages */}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
